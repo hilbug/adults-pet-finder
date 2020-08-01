bd6fb3577657cac8aad26932e0dac3397dca8834
Alissa Champigny <achampigny4@gmail.com>
2020-08-01 11:38:22 -0400

    cleaned non functioning code

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
-  $(".dropdown-trigger").dropdown();
-  $("select").formSelect();
-
     // input questions and options on search page
     $('select').attr("class", "browser-default");
     // mobile collapse nav bar on result page
     $('.sidenav').sidenav();
-
-
   });
