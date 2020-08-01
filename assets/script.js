d69f576091cd63f463ddbb917cd7534014064a8d
Alissa Champigny <achampigny4@gmail.com>
2020-08-01 12:35:45 -0400

    result and search container hide

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
-    // input questions and options on search page
-    $('select').attr("class", "browser-default");
-    // mobile collapse nav bar on result page
-    $('.sidenav').sidenav();
-  });
+  // input questions and options on search page
+  $('select').attr("class", "browser-default");
+  // mobile collapse nav bar on result page
+  // $('.sidenav').sidenav();
+
+  // hide result page
+  // $('#result').css('display', 'none');
+
+  // this code needs to be added within a on click of submit search button event
+  // // shows result
+  // $('#result').css('display', 'block');
+  //this can also be added to the submit function if we want to hide the search questions
+  // hide search
+  // $('#seachContainer').css('display', 'none');
+});
