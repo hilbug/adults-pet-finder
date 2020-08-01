03a4f32f9d18291f36fde8885b351d96e240d548
Alissa Champigny <achampigny4@gmail.com>
2020-08-01 10:15:36 -0400

    responsive materialize

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    // input questions and options on search page
+    $('select').attr("class", "browser-default");
+    // mobile collapse nav bar on result page
+    $('.sidenav').sidenav();
+
+
+  });
