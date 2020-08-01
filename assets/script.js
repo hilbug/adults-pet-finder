4c3a59cafe673e85d1b8fdd6eee1b590c59f26fd
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-01 10:40:18 -0400

    dog api call

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-// Test
+
+    
+    // API call to get dog picture
+    let dogQueryURL = "https://dog.ceo/api/breeds/image/random"
+    function dogPicture() {
+        $.ajax({
+            url: dogQueryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(dogQueryURL);
+                console.log(response);
+            });
+    }
+    // need to call the function
+    dogPicture();
