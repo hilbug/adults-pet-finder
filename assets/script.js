f90ebcb59f12cd1d8faed5a8e0b51209521addf3
hilbug <65197724+hilbug@users.noreply.github.com>
2020-08-01 11:45:16 -0400

    js add dog api

--- assets/script.js
+++ assets/script.js
@@ -2,24 +2,42 @@ $(document).ready(function () {
   $(".dropdown-trigger").dropdown();
   $("select").formSelect();
 
-    // input questions and options on search page
-    $('select').attr("class", "browser-default");
-    // mobile collapse nav bar on result page
-    $('.sidenav').sidenav();
+  // input questions and options on search page
+  $('select').attr("class", "browser-default");
+  // mobile collapse nav bar on result page
+  $('.sidenav').sidenav();
 
-     // API call to get dog picture
-     let dogQueryURL = "https://dog.ceo/api/breeds/image/random"
-     function dogPicture() {
-         $.ajax({
-             url: dogQueryURL,
-             method: "GET"
-         })
-             .then(function (response) {
-                 console.log(dogQueryURL);
-                 console.log(response);
-             });
-     }
-     // need to call the function
-     dogPicture();
+  // page variables
+  const breedDropdown = $("#breed");
 
-  });
+  // API call to get dog picture
+  let dogQueryURL = "https://dog.ceo/api/breeds/image/random";
+  let catQueryURL = "";
+  
+  let dogPicURL = "";
+  function dogPicture() {
+    $.ajax({
+      url: dogQueryURL,
+      method: "GET"
+    })
+      .then(function (response) {
+        console.log(dogQueryURL);
+        console.log(response)
+        dogPicURL = response.message;
+      });
+  }
+
+  // if user selects dog, call dog api
+  // function to populate the picture
+  function setPicture() {
+    let petImg = $(".card-image").children[0];
+    
+    if (breedDropdown.val() === 2) {
+      dogPicture();
+      petImg.attr("src", dogURL);
+    }
+  }
+
+  // listen for search click to populate the picture?
+  $(".card-action").on("click", setPicture);
+});
