dbf63f6a81ef1dc287ff0dc6f1aadaecf00d5afe
Alissa Champigny <achampigny4@gmail.com>
2020-08-02 16:43:06 -0400

    hide/show search and results

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
+  
+  //display script
   // input questions and options on search page
   $("select").attr("class", "browser-default");
   // mobile collapse nav bar on result page
   $('.sidenav').sidenav();
-
   // hide result page
-  // $('#result').css('display', 'none');
-
-  // this code needs to be added within a on click of submit search button event
-  // // shows result
-  // $('#result').css('display', 'block');
-  //this can also be added to the submit function if we want to hide the search questions
-  // hide search
-  // $('#seachContainer').css('display', 'none');
+  $('#result').css('display', 'none');
 
   // page variables
   const breedDropdown = $("#breed");
@@ -71,6 +65,10 @@ $(document).ready(function () {
     event.preventDefault();
     setPicture();
     console.log(event);
+    // hide search
+    $('#searchcontainer').hide();
+    // shows result
+    $('#result').css('display', 'block');
   });
 
   $("form.search-form").on("submit", function (event) {
